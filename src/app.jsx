b070874cabb80fb0690c64f946df8535070c0b14
oosok4 <oosok4@naver.com>
2021-06-02 15:19:45 +0900

    init project

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+//import React from 'react';
+import './app.css';
+
+function App() {
+  var name = undefined;
+  return (
+    <>
+      <h1>hello2</h1>
+
+    </>
+  );
+
+}
+
+export default App;
