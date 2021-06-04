109aeac709f919ff4bf94437aa2a718153052c8b
oosok4 <oosok4@naver.com>
2021-06-04 15:36:18 +0900

    show video with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,26 @@
 //import React from 'react';
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  var name = undefined;
-  return (
-    <>
-      <h1>hello2</h1>
+  const [videos, setVideos] = useState([]);
 
-    </>
-  );
+  useEffect(()=>{
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBu8Ybq4j_Ju38xXD3ZnmFpc8o_BAcmhbQ", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  } , []); // useEffect에서 저렇게 두번째 인자값으로 빈 배열을 입력하면
+           // 마운트가 되었을 때만 useEffect가 호출된다.
+
+  
+  return <VideoList videos = {videos}/>
 
 }
 
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+
+export default VideoItem;
+
+
+    // import React from 'react';
+
+// const VideoItem = (props) => <h1>{props.title}</h1>;
+
+// export default VideoItem;
